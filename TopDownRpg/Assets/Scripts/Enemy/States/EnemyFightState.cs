723dbab79ea04e787db71066a5d4548bc4683ed8
Bhawesh Agarwal <bhaweshagarwal02@gmail.com>
2023-08-19 19:34:24 +0530

    Fight to idel logic done

--- TopDownRpg/Assets/Scripts/Enemy/States/EnemyFightState.cs
+++ TopDownRpg/Assets/Scripts/Enemy/States/EnemyFightState.cs
@@ -1,11 +1,29 @@
 
 
+using UnityEngine;
+
 public class EnemyFightState :  EnemyState
 {
+    private float nextDetectionTime;
+
     public override void OnStateEnter()
     {
         base.OnStateEnter();
 
+    }
+    private void Start()
+    {
+        nextDetectionTime = Time.time;
+    }
+    private void Update()
+    {
+        if( Time.time < nextDetectionTime) 
+        {
+            return;
+        }
+        if (Model.PlayerTarget == null || Vector2.Distance(transform.position, Model.PlayerTarget.position) > Model.TargetReachedThersold)
+            View.ChangeState(View.IdelState);
+        nextDetectionTime = Time.time + Model.DetectionDelay;
     }
     public override void OnStateExit()
     {
