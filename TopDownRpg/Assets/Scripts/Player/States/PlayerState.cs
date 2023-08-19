af839a8b85f5ce13a429ed97e556d2ad158eefd9
Bhawesh Agarwal <bhaweshagarwal02@gmail.com>
2023-08-19 10:20:09 +0530

    Done

--- TopDownRpg/Assets/Scripts/Player/States/PlayerState.cs
+++ TopDownRpg/Assets/Scripts/Player/States/PlayerState.cs
@@ -8,7 +8,7 @@ public class PlayerState : MonoBehaviour
     protected PlayerView playerView;
     protected PlayerController playerController;
 
-    protected float nextSwingTime;
+
     private void Awake()
     {
         playerView = GetComponent<PlayerView>();
@@ -16,55 +16,13 @@ public class PlayerState : MonoBehaviour
     private void Start()
     {
         playerController = playerView.PlayerController;
-        nextSwingTime = Time.time;
     }
     public virtual void Update()
     {
-        playerView.HorizontalInput = Input.GetAxisRaw("Horizontal");
-        playerView.VerticalInput = Input.GetAxisRaw("Vertical");
-        changeLookDirectionBasedOnInput();
-        playerAttackCheck();
-
 
     }
 
-    private void playerAttackCheck()
-    {
-        if (Time.time >= nextSwingTime)
-        {
-            if (Input.GetMouseButton(0))
-            {
-                playerController.PlayerAttack();
-                nextSwingTime = Time.time + playerView.PlayerModel.SwingRate;
-            }
-            else
-            {
-                playerView.PlayerAnimator.SetBool("IsAttacking", false);
-            }
-        }
-    }
 
-    private void changeLookDirectionBasedOnInput()
-    {
-        if (playerView.HorizontalInput != 0)
-        {
-            if (playerView.HorizontalInput == 1)
-                playerController.ChangeLookDirection(LookDirection.Right);
-            else
-                playerController.ChangeLookDirection(LookDirection.Left);
-        }
-        else
-        {
-            if (playerView.VerticalInput == 1)
-            {
-                playerController.ChangeLookDirection(LookDirection.Up);
-            }
-            else if (playerView.VerticalInput == -1)
-            {
-                playerController.ChangeLookDirection(LookDirection.Down);
-            }
-        }
-    }
 
     public virtual void OnStateEnter()
     {
